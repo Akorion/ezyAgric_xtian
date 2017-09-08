0df870c1e0343083b2f98ecb024d0a012ec7d36c
kgidion1 <kgidion1@gmail.com>
2017-09-08 20:24:43 +0300

    correcting header file

--- include/header_client.php
+++ include/header_client.php
@@ -134,7 +134,7 @@ $path = $_SERVER["PHP_SELF"];
                         }
                         ?>
                         <li><a href="help.php">Help</a></li>
-                        <li><a href="../index.php?logout=1">Logout</a></li>
+                        <li><a href="./index.php?logout=1">Logout</a></li>
                     </ul>
                 </li>
 <!--                <li><a href="#" style="font-weight:800;">--><?php //if (isset($_SESSION["user_account"])) {
