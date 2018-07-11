730bf23b41e06075b3cbf133d7c5fb0d88809837
kgidion1 <kgidion1@gmail.com>
2018-07-11 16:04:34 +0300

    Update database_config.php

--- php_lib/config/database_config.php
+++ php_lib/config/database_config.php
@@ -10,8 +10,8 @@ public $_database_name;
 public function __construct(){
    $this->_server_name="localhost";
    $this->_user_name="root";
-   $this->_password="";
-   $this->_database_name="akorionc_ezyagric";
+   $this->_password="root";
+   $this->_database_name="akorionc";
    ini_set('max_execution_time', 300); //300 seconds = 5 minutes
    ini_set("auto_detect_line_endings", "1");   
 }
